f84c6c15bc6d2c61087a63fdae8a34805f8ce503
Luka Krapic <luka.krapic@gmail.com>
2022-06-16 08:49:21 +0200

    Add env variables to pytest init file
    Fixing bug with permissions for boto3. Not sure why fixture for
    aws environment does not work, investigate.
    
    Update pytest ini with aws env variables. Add function scope to the
    fixtures.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,16 +5,16 @@ import pytest
 from moto import mock_iam
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def aws_credentials():
-    """Mocked AWS Credentials for moto."""
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def iam_client(aws_credentials):
     with mock_iam():
         iam_client = boto3.client("iam")
